1eb3f0748cd6c1c1c44fd4e51fe2c2ea0169b527
Joshua Katz <gravypod@gravypod.com>
2014-05-12 23:40:51 -0400

    Allow for sectors to be compatible with YamlBeans

--- src/com/gravypod/starmadewrapper/Sector.java
+++ src/com/gravypod/starmadewrapper/Sector.java
@@ -6,18 +6,36 @@ package com.gravypod.starmadewrapper;
  * @author gravypod
  * 
  */
-public class Sector {
-	
-	private final int x, y, z;
+public class Sector implements Comparable<Sector> {
 	
 	/**
-	 * Generate a sector based on strings that represent numbers. All numbers are extracted by the <code>{@link Integer}.parseInt()</code> method
+	 * Public to allow serialization to yaml, do not use this to edit, use
+	 * getters and setters
+	 */
+	public int x, y, z;
+	
+	/**
+	 * Public to allow serialization to yaml, do not use this to edit, use
+	 * getters and setters
+	 */
+	public Sector() {
+	
+	}
+	
+	/**
+	 * Generate a sector based on strings that represent numbers. All numbers
+	 * are extracted by the <code>{@link Integer}.parseInt()</code> method
+	 * 
 	 * @param x
 	 * @param y
 	 * @param z
-	 * @throws Exception - NumberFormatException if the integers cannot be formated. It is casted down to force the end user to catch the exception.
+	 * @throws Exception
+	 *             - NumberFormatException if the integers cannot be formated.
+	 *             It is casted down to force the end user to catch the
+	 *             exception.
 	 */
 	public Sector(String x, String y, String z) throws Exception {
+	
 		int xPos, yPos, zPos;
 		try {
 			xPos = Integer.parseInt(x);
@@ -76,11 +94,34 @@ public class Sector {
 	
 	@Override
 	public boolean equals(Object arg0) {
-		
+	
 		if (!(arg0 instanceof Sector))
 			return super.equals(arg0);
 		Sector s = (Sector) arg0;
 		return s.x == x && s.y == y && s.z == z;
+	}
+	
+	@Override
+	public int compareTo(Sector s) {
+	
+		if (s.x == x && s.y == y && s.z == z) {
+			return 0;
+		}
+		
+		if (s.x >= x && s.y > y && s.z >= z) {
+			return 1;
+		}
+		
+		return -1;
+	}
+	
+	@Override
+	public int hashCode() {
+	
+		int result = (int) (x ^ (x >>> 32));
+		result = 31 * result + (int) (y ^ (y >>> 32));
+		result = 31 * result + (int) (z ^ (z >>> 32));
+		return result;
 	}
 	
 }
