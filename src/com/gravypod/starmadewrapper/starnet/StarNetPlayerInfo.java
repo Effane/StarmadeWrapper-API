af006debd63e72f40a7ac1affe22b6f38f2bd411
gravypod <gravypod@gravypod.com>
2014-08-10 00:14:29 -0400

    Clean code for player info integration. TODO: Clean decompiled code

--- src/com/gravypod/starmadewrapper/starnet/StarNetPlayerInfo.java
+++ src/com/gravypod/starmadewrapper/starnet/StarNetPlayerInfo.java
@@ -12,39 +12,44 @@ public class StarNetPlayerInfo {
 	private boolean upgraded;
 	private int credits;
 
-	private static final char NEW_LINE = '\n';
-	private static final String PLAYER_STRING = "Sector: %s " + NEW_LINE
-			+ "Username: %s (%s) " + NEW_LINE + "Upgraded: %b " + NEW_LINE
-			+ "Credits: %d";
+	private static final char NEW_LINE = '\n'; 
+	private static final String PLAYER_STRING = "Sector: %s " + NEW_LINE + "Username: %s (%s) " + NEW_LINE + "Upgraded: %b " + NEW_LINE + "Credits: %d";
 
 	public StarNetPlayerInfo(List<String> response) {
 
 		for (String s : response) {
 			
-			if (s.startsWith("[PL] FACTION: ")) {
-				faction = s.substring(s.indexOf(':') + 1).trim();
-				if (faction.equals("null")) {
-					faction = null;
-				}
+			if (!s.startsWith("[PL]")) {
+				continue;
 			}
-			if (s.startsWith("[PL] SECTOR: ")) {
-				sector = LocationUtils.sectorFromString(s);
-			}
-			if (s.startsWith("[PL] CREDITS: ")) {
-				credits = Integer.parseInt(s.substring(s.indexOf(':') + 1).trim());
-			}
-			if (s.startsWith("[PL] Name: ")) {
-				username = s.substring(s.indexOf(':') + 1).trim();
-			}
-
-			if (s.startsWith("[PL] IP: ")) {
-				ip = s.substring(s.indexOf(':') + 1).trim();
-			}
-			if (s.startsWith("[PL] SM-NAME: ")) {
-				smName = s.substring(s.indexOf(':') + 1).trim();
-			}
-			if (s.startsWith("[PL] UPGRADED: ")) {
-				upgraded = Boolean.valueOf(s.substring(s.indexOf(':') + 1).trim());
+			
+			String type = s.substring(s.indexOf(' ') + 1, s.indexOf(':'));
+			String payload = s.substring(s.indexOf(':') + 1).trim();
+			
+			switch (type) { // TODO: Login messages
+				case "FACTION":
+					if (payload.equals("null")) {
+						faction = null;
+					}
+					break;
+				case "SECTOR":
+					sector = LocationUtils.sectorFromString(s);
+					break;
+				case "CREDITS":
+					credits = Integer.parseInt(payload);
+					break;
+				case "IP":
+					ip = payload;
+					break;
+				case "Name":
+					username = payload;
+					break;
+				case "SM-NAME":
+					smName = payload;
+					break;
+				case "UPGRADED":
+					upgraded = Boolean.valueOf(payload);
+					break;
 			}
 		}
 
@@ -74,18 +79,13 @@ public class StarNetPlayerInfo {
 		return username;
 	}
 
-	public static char getNewLine() {
-		return NEW_LINE;
-	}
-
 	public boolean isUpgraded() {
 		return upgraded;
 	}
 
 	@Override
 	public String toString() {
-		return String.format(PLAYER_STRING, getSector().toString(),
-				getUsername(), getSmName(), isUpgraded(), getCredits());
+		return String.format(PLAYER_STRING, getSector().toString(), getUsername(), getSmName(), isUpgraded(), getCredits());
 	}
 
 }
